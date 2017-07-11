f90926b198da346940dc154e38cc1bbfa3d45a8a
Ju-Hang Sin <juhangsin@gmail.com>
2017-07-11 17:28:32 -0400

    part 1 completed

--- election.js
+++ election.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+var list = document.getElementById( 'unorder_list' );
+
+	$.ajax({
+		url: 'https://bb-election-api.herokuapp.com/',
+		method: 'GET',
+		dataType: 'json',
+	}).done(function(responseData) {
+		console.log(responseData);
+		responseData.candidates.forEach(function(info) {
+			var voter = document.createElement('li');
+			voter.innerHTML = 'NAME: ' + info.name + 'VOTES: ' + info.votes
+			list.append(voter)
+		})
+	})
 
 });
